468bc71359474c616e3f576e90c328145ed07364
lahrach el mehdi <lahrachelmehdi2025@gmail.com>
2025-11-06 15:55:57 +0100

    ajouter une menu mobile dans tou les page

--- scripte.js
+++ scripte.js
@@ -1,12 +1,21 @@
-const open_menu = document.querySelector("#op")
-const close_menu = document.querySelector("#cl")
-// function menu(){
+// const open_menu = document.querySelector("#op")
+// const close_menu = document.querySelector("#cl")
+// // function menu(){
 
-//     open_menu.classList.toggle("hidden")
-//     close_menu.classList.toggle("hidden")
-// }
+// //     open_menu.classList.toggle("hidden")
+// //     close_menu.classList.toggle("hidden")
+// // }
 
-console.log(open_menu,close_menu)
+// console.log(open_menu,close_menu)
 
 
-        var swiper = new Swiper(".mySwiper", {});
+var swiper = new Swiper(".mySwiper", {});
+
+
+
+const menuHamburger = document.querySelector(".open_menu")
+const navLinks = document.querySelector(".open")
+
+menuHamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('mobile-menu')
+})
