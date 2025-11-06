f5cce1c28adfa4ea55c7ed45e4c1ff5bd50fc64f
lahrach el mehdi <lahrachelmehdi2025@gmail.com>
2025-11-06 19:43:39 +0100

    ajouter la pagination au market

--- scripte.js
+++ scripte.js
@@ -73,6 +73,147 @@ const product = [
         },
 ]
 
+
+const product2 =[
+        {
+                name: 'lopi',
+                price: 20,
+                image: 'image/carte-10.png',
+                color: 'green'
+        },
+        {
+                name: 'mio',
+                price: 20,
+                image: 'image/carte-11.png',
+                color: 'orange'
+        },
+       
+
+]
+
+const btn2 = document.querySelector("#btn2")
+btn2.addEventListener('click', () => {
+        product.forEach((el) => {
+        carte_market.innerHTML = `
+    
+    `
+})
+        product2.forEach((el) => {
+        carte_market.innerHTML += `
+    
+    <div
+                class="carte_market w-65 h-108 lg:w-90 lg:h-150 rounded-[15px] flex flex-col justify-center items-center gap-5">
+                <div
+                    class=" bg-[url(${el.image})] bg-cover bg-center lg:w-80 w-60 lg:h-112 h-85 flex flex-col justify-end items-center">
+                    <div
+                        class=" carte text-white relative flex flex-col justify-center items-center bottom-0.5  w-full h-33 lg:h-44 rounded-[10px]">
+                        <div class="flex justify-center items-center lg:gap-6 gap-6 relative left-2.5">
+                            <h2 class="anime rotate-[90deg] lg:text-[24px] text-[18px] absolute  lg:right-44 right-35 ">
+                                ANIME</h2>
+                                <div
+                                    class="flex flex-col font-[Poppins] gap-1 w-25 lg:w-35 relative lg:left-6 left-2 lg:gap-1">
+                                    <h5 class="lg:text-[15px] text-[12px] font-[700]">${el.name}</h5>
+                                    <p class="lg:text-[12px]/3 text-[9px]/2  font-[400] ">Lorem ipsum dolor
+                                        sit
+                                        ame Lorem ipsum
+                                        dolor
+                                        sit
+                                        amet
+                                        consectetur adipisicing elit.</p>
+                                </div>
+                                <P class="lg:text-[11px] text-[9px] font-[700] relative top-1">
+                                    prix : ${el.price}$
+                                </P>
+                        </div>
+                        <div class="flex gap-8 lg:gap-13 lg:text-[24px] text-[20px] font-[700] font-[Lemon]">
+                            <h2>POKEMON</h2>
+                            <h3 class="text-${el.color}-500">XX</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex gap-12">
+                    <button
+                        class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#2F8E44]">ajouter
+                        au
+                        panie</button>
+                    <button
+                        class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#936C57]">ajouter
+                        aux
+                        favoris</button>
+                </div>
+            </div>
+     
+    
+    `
+})
+        
+})
+
+
+const btn1 = document.querySelector("#btn1")
+btn1.addEventListener('click', () => {
+        product2.forEach((el) => {
+        carte_market.innerHTML = `
+    
+    `
+})
+        product.forEach((el) => {
+        carte_market.innerHTML += `
+    
+    <div
+                class="carte_market w-65 h-108 lg:w-90 lg:h-150 rounded-[15px] flex flex-col justify-center items-center gap-5">
+                <div
+                    class=" bg-[url(${el.image})] bg-cover bg-center lg:w-80 w-60 lg:h-112 h-85 flex flex-col justify-end items-center">
+                    <div
+                        class=" carte text-white relative flex flex-col justify-center items-center bottom-0.5  w-full h-33 lg:h-44 rounded-[10px]">
+                        <div class="flex justify-center items-center lg:gap-6 gap-6 relative left-2.5">
+                            <h2 class="anime rotate-[90deg] lg:text-[24px] text-[18px] absolute  lg:right-44 right-35 ">
+                                ANIME</h2>
+                                <div
+                                    class="flex flex-col font-[Poppins] gap-1 w-25 lg:w-35 relative lg:left-6 left-2 lg:gap-1">
+                                    <h5 class="lg:text-[15px] text-[12px] font-[700]">${el.name}</h5>
+                                    <p class="lg:text-[12px]/3 text-[9px]/2  font-[400] ">Lorem ipsum dolor
+                                        sit
+                                        ame Lorem ipsum
+                                        dolor
+                                        sit
+                                        amet
+                                        consectetur adipisicing elit.</p>
+                                </div>
+                                <P class="lg:text-[11px] text-[9px] font-[700] relative top-1">
+                                    prix : ${el.price}$
+                                </P>
+                        </div>
+                        <div class="flex gap-8 lg:gap-13 lg:text-[24px] text-[20px] font-[700] font-[Lemon]">
+                            <h2>POKEMON</h2>
+                            <h3 class="text-${el.color}-500">XX</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex gap-12">
+                    <button
+                        class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#2F8E44]">ajouter
+                        au
+                        panie</button>
+                    <button
+                        class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#936C57]">ajouter
+                        aux
+                        favoris</button>
+                </div>
+            </div>
+     
+    
+    `
+})
+        
+})
+
+
+////////////////////////////////
+
+
+
+
 const filtrage = [
         {
                 name: 'Communes',
