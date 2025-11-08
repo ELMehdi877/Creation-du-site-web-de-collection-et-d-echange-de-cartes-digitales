1131b4d75a94d52d9c479d2ba4a83912e34e1896
lahrach el mehdi <lahrachelmehdi2025@gmail.com>
2025-11-08 19:46:22 +0100

    modification au niveau de filtrage mobile et disktop

---
+++ script_test.js
@@ -0,0 +1,27 @@
+// const my_parent = document.getElementById('parent')
+// my_parent.innerHTML = ('hello')
+// const para = document.createElement('p')
+// para.innerHTML = ('hello word')
+// my_parent.appendChild(para)
+// const p1 = document.getElementById('p1')
+// p1.style.color = "red"
+// p1.style.border='solid red 1px'
+// const p2 = document.getElementById('p2')
+// p2.style.fontSize= "18px" 
+
+// const my_nom = document.getElementById('nom')
+// function affiche(){
+// alert(my_nom.value);
+
+// }
+
+
+// localStorage.removeItem("color"
+localStorage.clear()
+// localStorage.name='[]'
+// localStorage.setItem
+
+// document.body.style.background=localStorage.getItem('color')
+// console.log(localStorage);
+// console.log(typeof ocalStorage);
+
--- scripte.js
+++ scripte.js
@@ -9,84 +9,101 @@ menuHamburger.addEventListener('click', () => {
         navLinks.classList.toggle('mobile-menu')
 })
 
+const openRarter = document.querySelector(".open_rarter")
+const Rarter = document.querySelector(".rarter")
+openRarter.addEventListener('click',() => {
+        Rarter.classList.toggle('open_rarter')
+})
 
 
 const product = [
         {
+                id: 1,
                 name: 'poki',
                 price: 20,
                 image: 'image/carte-6.png',
                 color: 'orange'
         },
         {
+                id: 2,
                 name: 'polo',
                 price: 10,
                 image: 'image/carte-1.png',
                 color: 'purple'
         },
         {
+                id: 3,
                 name: 'kiki',
                 price: 15,
                 image: 'image/carte-5.png',
                 color: 'green'
         },
         {
+                id: 4,
                 name: 'lolo',
                 price: 20,
                 image: 'image/carte-2.png',
                 color: 'orange'
         },
         {
+                id: 5,
                 name: 'ninii',
                 price: 20,
                 image: 'image/carte-3.png',
                 color: 'yellow'
         },
         {
+                id: 6,
                 name: 'nayno',
                 price: 20,
                 image: 'image/carte-4.png',
                 color: 'blue'
         },
-        // {
-        //         name: 'lopi',
-        //         price: 20,
-        //         image: 'image/carte-10.png',
-        //         color: 'green'
-        // },
-        // {
-        //         name: 'mio',
-        //         price: 20,
-        //         image: 'image/carte-11.png',
-        //         color: 'orange'
-        // },
-        // {
-        //         name: 'hola',
-        //         price: 20,
-        //         image: 'image/carte-12.png',
-        //         color: 'orange'
-        // },
-        // {
-        //         name: 'gigo',
-        //         price: 18,
-        //         image: 'image/carte-9.png',
-        //         color: 'yellow'
-        // },
-        // {
-        //         name: 'poha',
-        //         price: 50,
-        //         image: 'image/carte-8.png',
-        //         color: 'blue'
-        // },
-        // {
-        //         name: 'fiane',
-        //         price: 2,
-        //         image: 'image/carte-7.png',
-        //         color: 'orange'
-        // },
+        {
+                id: 7,
+                name: 'lopi',
+                price: 20,
+                image: 'image/carte-10.png',
+                color: 'green'
+        },
+        {
+                id: 8,
+                name: 'mio',
+                price: 20,
+                image: 'image/carte-11.png',
+                color: 'orange'
+        },
+        {
+                id: 9,
+                name: 'hola',
+                price: 20,
+                image: 'image/carte-12.png',
+                color: 'orange'
+        },
+        {
+                id: 10,
+                name: 'gigo',
+                price: 18,
+                image: 'image/carte-9.png',
+                color: 'yellow'
+        },
+        {
+                id: 11,
+                name: 'poha',
+                price: 50,
+                image: 'image/carte-8.png',
+                color: 'blue'
+        },
+        {
+                id: 12,
+                name: 'fiane',
+                price: 2,
+                image: 'image/carte-7.png',
+                color: 'orange'
+        },
 
 ];
- 
+
 
 
 const barre_filtrage = [
@@ -114,6 +131,7 @@ const barre_filtrage = [
 const buttons_filtrage = document.querySelector('#filtrage')
 
 barre_filtrage.forEach((el) => {
+        // if(buttons_filtrage)
         buttons_filtrage.innerHTML += `
         <li id="btn_${el.color}" class=" bg-${el.color}-500  text-black w-35 flex justify-center rounded-tl-[10px] rounded-br-[10px]">
                     ${el.name}</li>
@@ -122,10 +140,10 @@ barre_filtrage.forEach((el) => {
 })
 
 barre_filtrage.forEach((el) => {
-        console.log(el);
-        if(!el)return;
+        // console.log(el);
+        if (!el) return;
         const color_btn = document.getElementById(`btn_${el.color}`);
-        console.log(color_btn)
+        // console.log(color_btn)
         color_btn.addEventListener('click', () => {
                 afiche_carte(el.color)
 
@@ -139,205 +157,44 @@ const copy_product = [
 ]
 
 
-product.forEach((el)=>{
+product.forEach((el) => {
 
         copy_product.push(el)
 })
 
 
-const product2 = [
-        {
-                name: 'lopi',
-                price: 20,
-                image: 'image/carte-10.png',
-                color: 'green'
-        },
-        {
-                name: 'mio',
-                price: 20,
-                image: 'image/carte-11.png',
-                color: 'orange'
-        },
-        {
-                name: 'hola',
-                price: 20,
-                image: 'image/carte-12.png',
-                color: 'orange'
-        },
-        {
-                name: 'gigo',
-                price: 18,
-                image: 'image/carte-9.png',
-                color: 'yellow'
-        },
-        {
-                name: 'poha',
-                price: 50,
-                image: 'image/carte-8.png',
-                color: 'blue'
-        },
-        {
-                name: 'fiane',
-                price: 2,
-                image: 'image/carte-7.png',
-                color: 'orange'
-        },
+// localStorage favoris
+let tous_favoris = JSON.parse(localStorage.getItem('favoris')) || []
+console.log(tous_favoris);
 
-];
+function ajoute_favoris(id) {
+        // console.log( product.find(el => el.id === id));
+        const el_favoris = product.find(el => el.id === id);
+        const isExist = tous_favoris.some(element => element.id === id)
+        if (!isExist) {
+                tous_favoris.push(el_favoris)
+                console.log(tous_favoris);
+        }
+        else {
+                return tous_favoris
+        }
+        // localStorage.removeItem("favoris")
+        localStorage.setItem("favoris", JSON.stringify(tous_favoris))
 
-const btn2 = document.querySelector("#btn2")
-btn2.addEventListener('click', () => {
-        product.forEach((el) => {
-                carte_market.innerHTML = `
-    
-    `
-        })
-        product2.forEach((el) => {
-                carte_market.innerHTML += `
-    
-    <div
-                class="carte_market w-65 h-108 lg:w-90 lg:h-150 rounded-[15px] flex flex-col justify-center items-center gap-5">
-                <div
-                    class=" bg-[url(${el.image})] bg-cover bg-center lg:w-80 w-60 lg:h-112 h-85 flex flex-col justify-end items-center">
-                    <div
-                        class=" carte text-white relative flex flex-col justify-center items-center bottom-0.5  w-full h-33 lg:h-44 rounded-[10px]">
-                        <div class="flex justify-center items-center lg:gap-6 gap-6 relative left-2.5">
-                            <h2 class="anime rotate-[90deg] lg:text-[24px] text-[18px] absolute  lg:right-44 right-35 ">
-                                ANIME</h2>
-                                <div
-                                    class="flex flex-col font-[Poppins] gap-1 w-25 lg:w-35 relative lg:left-6 left-2 lg:gap-1">
-                                    <h5 class="lg:text-[15px] text-[12px] font-[700]">${el.name}</h5>
-                                    <p class="lg:text-[12px]/3 text-[9px]/2  font-[400] ">Lorem ipsum dolor
-                                        sit
-                                        ame Lorem ipsum
-                                        dolor
-                                        sit
-                                        amet
-                                        consectetur adipisicing elit.</p>
-                                </div>
-                                <P class="lg:text-[11px] text-[9px] font-[700] relative top-1">
-                                    prix : ${el.price}$
-                                </P>
-                        </div>
-                        <div class="flex gap-8 lg:gap-13 lg:text-[24px] text-[20px] font-[700] font-[Lemon]">
-                            <h2>POKEMON</h2>
-                            <h3 class="text-${el.color}-500">XX</h3>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex gap-12">
-                    <button
-                        class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#2F8E44]">ajouter
-                        au
-                        panie</button>
-                    <button
-                        class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#936C57]">ajouter
-                        aux
-                        favoris</button>
-                </div>
-            </div>
-     
-    
-    `
-        })
-
-})
-
-
-const btn1 = document.querySelector("#btn1")
-btn1.addEventListener('click', () => {
-        product2.forEach((el) => {
-                carte_market.innerHTML = `
-    
-    `
-        })
-        product.forEach((el) => {
-                carte_market.innerHTML += `
-    
-    <div
-                class="carte_market w-65 h-108 lg:w-90 lg:h-150 rounded-[15px] flex flex-col justify-center items-center gap-5">
-                <div
-                    class=" bg-[url(${el.image})] bg-cover bg-center lg:w-80 w-60 lg:h-112 h-85 flex flex-col justify-end items-center">
-                    <div
-                        class=" carte text-white relative flex flex-col justify-center items-center bottom-0.5  w-full h-33 lg:h-44 rounded-[10px]">
-                        <div class="flex justify-center items-center lg:gap-6 gap-6 relative left-2.5">
-                            <h2 class="anime rotate-[90deg] lg:text-[24px] text-[18px] absolute  lg:right-44 right-35 ">
-                                ANIME</h2>
-                                <div
-                                    class="flex flex-col font-[Poppins] gap-1 w-25 lg:w-35 relative lg:left-6 left-2 lg:gap-1">
-                                    <h5 class="lg:text-[15px] text-[12px] font-[700]">${el.name}</h5>
-                                    <p class="lg:text-[12px]/3 text-[9px]/2  font-[400] ">Lorem ipsum dolor
-                                        sit
-                                        ame Lorem ipsum
-                                        dolor
-                                        sit
-                                        amet
-                                        consectetur adipisicing elit.</p>
-                                </div>
-                                <P class="lg:text-[11px] text-[9px] font-[700] relative top-1">
-                                    prix : ${el.price}$
-                                </P>
-                        </div>
-                        <div class="flex gap-8 lg:gap-13 lg:text-[24px] text-[20px] font-[700] font-[Lemon]">
-                            <h2>POKEMON</h2>
-                            <h3 class="text-${el.color}-500">XX</h3>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex gap-12">
-                    <button
-                        class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#2F8E44]">ajouter
-                        au
-                        panie</button>
-                    <button
-                        class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#936C57]">ajouter
-                        aux
-                        favoris</button>
-                </div>
-            </div>
-     
-    
-    `
-        })
-
-})
-
-
-////////////////////////////////
-
-const copy_product2 = [
-
-]
-product2.forEach((el)=>{
-        copy_product2.push(el)
-})
-
-copy_product2.forEach((el)=>{
-        copy_product.push(el)
-})
+}
 
 
 
-
-
-
-
-const carte_market = document.querySelector('#carte_market')
-console.log(carte_market)
-
-
-
-
-
+const carte_market = document.getElementById('cartes_market')
 
 function afiche_carte(el) {
-        console.log(el);
-        carte_market.innerHTML=''
+        // console.log(el);
+        carte_market.innerHTML = ''
         // if(!el)return;
-        const color = el ===undefined ? 'all':el
-        console.log(color);
-        const filter_product=copy_product.filter(el=>el.color===color || color==='all')
-        console.log(filter_product)
+        const color = el === undefined ? 'all' : el
+        // console.log(color);
+        const filter_product = copy_product.filter(el => el.color === color || color === 'all')
+        // console.log(filter_product)
         filter_product.forEach((el) => {
                 carte_market.innerHTML += `
     
@@ -372,11 +229,11 @@ function afiche_carte(el) {
                     </div>
                 </div>
                 <div class="flex gap-12">
-                    <button
+                    <button 
                         class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#2F8E44]">ajouter
                         au
                         panie</button>
-                    <button
+                    <button onclick="ajoute_favoris(${el.id})"
                         class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#936C57]">ajouter
                         aux
                         favoris</button>
@@ -387,11 +244,66 @@ function afiche_carte(el) {
     `
         })
 }
-// afiche_carte()
+afiche_carte()
 
 
-product.forEach((el)=>{
-        carte_market.innerHTML += `
+// product.forEach((el)=>{
+//         carte_market.innerHTML += `
+
+//     <div
+//                 class="carte_market w-65 h-108 lg:w-90 lg:h-150 rounded-[15px] flex flex-col justify-center items-center gap-5">
+//                 <div
+//                     class=" bg-[url(${el.image})] bg-cover bg-center lg:w-80 w-60 lg:h-112 h-85 flex flex-col justify-end items-center">
+//                     <div
+//                         class=" carte text-white relative flex flex-col justify-center items-center bottom-0.5  w-full h-33 lg:h-44 rounded-[10px]">
+//                         <div class="flex justify-center items-center lg:gap-6 gap-6 relative left-2.5">
+//                             <h2 class="anime rotate-[90deg] lg:text-[24px] text-[18px] absolute  lg:right-44 right-35 ">
+//                                 ANIME</h2>
+//                                 <div
+//                                     class="flex flex-col font-[Poppins] gap-1 w-25 lg:w-35 relative lg:left-6 left-2 lg:gap-1">
+//                                     <h5 class="lg:text-[15px] text-[12px] font-[700]">${el.name}</h5>
+//                                     <p class="lg:text-[12px]/3 text-[9px]/2  font-[400] ">Lorem ipsum dolor
+//                                         sit
+//                                         ame Lorem ipsum
+//                                         dolor
+//                                         sit
+//                                         amet
+//                                         consectetur adipisicing elit.</p>
+//                                 </div>
+//                                 <P class="lg:text-[11px] text-[9px] font-[700] relative top-1">
+//                                     prix : ${el.price}$
+//                                 </P>
+//                         </div>
+//                         <div class="flex gap-8 lg:gap-13 lg:text-[24px] text-[20px] font-[700] font-[Lemon]">
+//                             <h2>POKEMON</h2>
+//                             <h3 class="text-${el.color}-500">XX</h3>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div class="flex gap-12">
+//                     <button
+//                         class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#2F8E44]">ajouter
+//                         au
+//                         panie</button>
+//                     <button id="favoris-btn"
+//                         class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#936C57]" onclick="addToFavorite(${el.id})">ajouter
+//                         aux
+//                         favoris</button>
+//                 </div>
+//             </div>
+
+
+//     `
+// })
+
+
+
+const Section_favoris = document.getElementById('section_favoris')
+console.log(Section_favoris);
+tous_favoris.forEach((el) => {
+        Section_favoris.innerHTML = ''
+        if(Section_favoris)
+        Section_favoris.innerHTML += `
     
     <div
                 class="carte_market w-65 h-108 lg:w-90 lg:h-150 rounded-[15px] flex flex-col justify-center items-center gap-5">
@@ -424,11 +336,11 @@ product.forEach((el)=>{
                     </div>
                 </div>
                 <div class="flex gap-12">
-                    <button
+                    <button 
                         class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#2F8E44]">ajouter
                         au
                         panie</button>
-                    <button
+                    <button onclick="ajoute_favoris(${el.id})"
                         class="w-18 h-10 lg:w-22 lg:h-13 rounded-[4px] text-[12px] lg:text-[15px] text-white bg-[#936C57]">ajouter
                         aux
                         favoris</button>
@@ -437,4 +349,5 @@ product.forEach((el)=>{
      
     
     `
+
 })
